227237490e78f0f25e7b8c9c63a51842886f89c7
db1-igorsilva <igor.silva@db1.com.br>
2019-07-10 13:05:57 -0300

    Setters adicionados à classe Entidade

--- app/src/main/java/com/igorhenss/killNdice/domain/Entidade.java
+++ app/src/main/java/com/igorhenss/killNdice/domain/Entidade.java
@@ -16,4 +16,39 @@ public class Entidade {
     Integer iniciativa;
     Integer inteligencia;
 
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setAlinhamento(Alinhamento alinhamento) {
+        this.alinhamento = alinhamento;
+    }
+
+    public void setHitpoints(Integer hitpoints) {
+        this.hitpoints = hitpoints;
+    }
+
+    public void setCarisma(Integer carisma) {
+        this.carisma = carisma;
+    }
+
+    public void setConstituicao(Integer constituicao) {
+        this.constituicao = constituicao;
+    }
+
+    public void setDestreza(Integer destreza) {
+        this.destreza = destreza;
+    }
+
+    public void setForca(Integer forca) {
+        this.forca = forca;
+    }
+
+    public void setIniciativa(Integer iniciativa) {
+        this.iniciativa = iniciativa;
+    }
+
+    public void setInteligencia(Integer inteligencia) {
+        this.inteligencia = inteligencia;
+    }
 }
