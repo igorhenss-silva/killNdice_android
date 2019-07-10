649f8e132e3df749b331ef56015b3ebd926a76f0
db1-igorsilva <igor.silva@db1.com.br>
2019-07-10 12:35:36 -0300

    Alinhamentos básicos adicionados.

--- app/src/main/java/com/igorhenss/killNdice/enums/Alinhamento.java
+++ app/src/main/java/com/igorhenss/killNdice/enums/Alinhamento.java
@@ -1,4 +1,7 @@
 package com.igorhenss.killNdice.enums;
 
 public enum Alinhamento {
+    LEAL_BOM, LEAL_NEUTRO, LEAL_MAU,
+    NEUTRO_BOM, NEUTRO_NEUTRO, NEUTRO_MAU,
+    CAOTICO_BOM, CAOTICO_NEUTRO, CAOTICO_MAU
 }
