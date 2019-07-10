8931b5ea3e0b1adff4189cf9467f058e3ff52644
db1-igorsilva <igor.silva@db1.com.br>
2019-07-10 12:44:42 -0300

    Atributos Classe e Profissao transferidos da classe Entidade

--- app/src/main/java/com/igorhenss/killNdice/domain/Personagem.java
+++ app/src/main/java/com/igorhenss/killNdice/domain/Personagem.java
@@ -1,4 +1,11 @@
 package com.igorhenss.killNdice.domain;
 
+import com.igorhenss.killNdice.enums.Classe;
+import com.igorhenss.killNdice.enums.Profissao;
+
 public class Personagem extends Entidade {
+
+    Classe classe;
+    Profissao profissao;
+
 }
