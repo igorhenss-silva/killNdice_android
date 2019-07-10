2ab637df6d4b3b5ce61fe4cb84f02a36f1693875
db1-igorsilva <igor.silva@db1.com.br>
2019-07-10 13:44:10 -0300

    Atributos gerais movidos para a nova classe AtributosGerais.

---
+++ app/src/main/java/com/igorhenss/killNdice/domain/AtributosGerais.java
@@ -0,0 +1,40 @@
+package com.igorhenss.killNdice.domain;
+
+public class AtributosGerais {
+
+    Integer carisma;
+    Integer constituicao;
+    Integer destreza;
+    Integer forca;
+    Integer iniciativa;
+    Integer inteligencia;
+
+    public AtributosGerais() { }
+
+    // SETTERS
+
+    protected void setCarisma(Integer carisma) {
+        this.carisma = carisma;
+    }
+
+    protected void setConstituicao(Integer constituicao) {
+        this.constituicao = constituicao;
+    }
+
+    protected void setDestreza(Integer destreza) {
+        this.destreza = destreza;
+    }
+
+    protected void setForca(Integer forca) {
+        this.forca = forca;
+    }
+
+    protected void setIniciativa(Integer iniciativa) {
+        this.iniciativa = iniciativa;
+    }
+
+    protected void setInteligencia(Integer inteligencia) {
+        this.inteligencia = inteligencia;
+    }
+
+}
