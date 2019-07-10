9a52afc8326b5d219ba4386c94204fddda2dbae7
db1-igorsilva <igor.silva@db1.com.br>
2019-07-10 13:45:18 -0300

    Classe Personagem em desenvolvimento.

--- app/src/main/java/com/igorhenss/killNdice/domain/Personagem.java
+++ app/src/main/java/com/igorhenss/killNdice/domain/Personagem.java
@@ -15,24 +15,15 @@ public class Personagem extends Entidade {
 
     // CONSTRUTORES
 
-    public Personagem() { }
+    public Personagem() {
+
+    }
 
     public void atributosBasicos(String nome, Alinhamento alinhamento,
                                  Integer hitpoints) {
         setNome(nome);
         setAlinhamento(alinhamento);
         setHitpoints(hitpoints);
-    }
-
-    public void atributosGerais(Integer carisma, Integer constituicao,
-                                Integer destreza, Integer forca,
-                                Integer iniciativa, Integer inteligencia) {
-        setCarisma(carisma);
-        setConstituicao(constituicao);
-        setDestreza(destreza);
-        setForca(forca);
-        setIniciativa(iniciativa);
-        setInteligencia(inteligencia);
     }
 
     public void atributosDePersonagem(Raca raca, Classe classe,
