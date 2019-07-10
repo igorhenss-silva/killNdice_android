5ca9c75dd1357cd4f77ac6b4aff6cf2da39a5289
db1-igorsilva <igor.silva@db1.com.br>
2019-07-10 12:41:57 -0300

    Atributos adicionados à Entidade

--- app/src/main/java/com/igorhenss/killNdice/domain/Entidade.java
+++ app/src/main/java/com/igorhenss/killNdice/domain/Entidade.java
@@ -1,10 +1,23 @@
 package com.igorhenss.killNdice.domain;
 
+import com.igorhenss.killNdice.enums.Alinhamento;
+import com.igorhenss.killNdice.enums.Classe;
+import com.igorhenss.killNdice.enums.Profissao;
+
 public class Entidade {
 
     String nome;
+
+    Alinhamento alinhamento;
+    Classe classe;
+    Profissao profissao;
     Integer hitpoints;
-    Integer forca;
+
+    Integer carisma;
     Integer constituicao;
+    Integer destreza;
+    Integer forca;
+    Integer iniciativa;
+    Integer inteligencia;
 
 }
